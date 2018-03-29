93e22f8ccea6aa9d2a22296d934753cd186aaa66
Michael Morris <mdam2345@gmail.com>
2018-03-29 01:15:06 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,11 +1,4 @@
 
-def display_board(board)
-    puts "   |   |   "
-    puts "-----------"
-    puts "   |   |   "
-    puts "-----------"
-    puts "   |   |   "
-end
 
 def display_board(board)
     puts " #{board[0]} | #{board[1]} | #{board[2]} "
