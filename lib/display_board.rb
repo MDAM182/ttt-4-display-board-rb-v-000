67fe6d9da95f6a8e8a60294e3e4369424f9e2187
Michael Morris <mdam2345@gmail.com>
2018-03-29 01:07:03 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -6,12 +6,3 @@ def display_board(board)
     puts "-----------"
     puts "   |   |   "
 end
-
-def display_board(board)
-    board[4] === "X"
-    puts "#{board[0]}|#{board[1]}|#{board[2]}"
-    puts             "-----------"
-    puts "#{board[3]}|#{board[4]}|#{board[5]}"
-    puts             "-----------"
-    puts "#{board[6]}|#{board[7]}|#{board[8]}"
-    end
